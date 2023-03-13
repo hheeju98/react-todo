353d7cea7fe10fe19bb5e7ba788c7d98137b60d8
hheeju98 <hheeju98@naver.com>
2023-03-14 00:32:00 +0900

    버그수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,6 +5,8 @@ import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
   const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
+  // const [todos, setTodos] = useState(readTodosFromLocalStorage);
+  // const [todos, setTodos] = useState(readTodosFromLocalStorage());
 
   const handleAdd = (todo) => {
     setTodos([...todos, todo]);
